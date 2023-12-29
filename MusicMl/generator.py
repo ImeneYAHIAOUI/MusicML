786f7deaae5aa8ce04cde95b191944b84668fe5c
ImeneYAHIAOUI <90778346+ImeneYAHIAOUI@users.noreply.github.com>
2023-12-29 21:42:13 +0100

    add midi regions

--- MusicMl/generator.py
+++ MusicMl/generator.py
@@ -332,6 +332,8 @@ def compile_track(music_ml_model, music_ml_meta, track, midi_file, track_number)
             if repeat == 0:
                 repeat = 1
             i += repeat
+    for region in track.midiRegions:
+        compile_region(music_ml_model, music_ml_meta, region, midi_file, track, track_number, channel, velocity)
 
 
 def compile_bar(music_ml_model, music_ml_meta, bar, i, midi_file, track, track_number, channel, velocity):
@@ -393,6 +395,61 @@ def get_original_bar(music_ml_meta, track, bar):
     if original_bar is None:
         raise TextXSemanticError('Reused bar not found: ' + bar.name, **get_location(bar.name))
     return original_bar
+
+
+def get_original_region(music_ml_meta, track, region):
+    original_region = None
+    for r in track.midiRegions:
+        if textx_isinstance(r, music_ml_meta['Region']) and r.name == region.ref:
+            original_region = r
+            break
+    if original_region is None:
+        raise TextXSemanticError('Reused region not found: ' + region.name, **get_location(region.name))
+    return original_region
+
+
+def compile_region(music_ml_model, music_ml_meta, region, midi_file, track, track_number, channel, velocity):
+
+    if textx_isinstance(region, music_ml_meta['ReusedRegion']):
+        if region.velocity != 0:
+            velocity = region.velocity
+        original_region = get_original_region(music_ml_meta, track, region)
+        original_regions_start_position = original_region.start
+
+        original_regions_end_position = original_regions_start_position + original_region.size
+
+        reused_start_position = region.start
+        track_index = track_number
+        if midi_file.header.numeric_format == 1:
+            track_index += 1
+        midi_events = [event for event in midi_file.tracks[track_index].eventList if event.evtname == 'NoteOn' and
+                       (original_regions_start_position <= event.tick < original_regions_end_position
+                        or original_regions_start_position <= event.tick +
+                        event.duration < original_regions_end_position
+                        or (
+                                event.tick <= original_regions_start_position and event.tick
+                                + event.duration >= original_regions_end_position))]
+        for midi_event in midi_events:
+            start = midi_event.tick
+            duration = midi_event.duration
+            reused_event_start_position = reused_start_position
+            if start < original_regions_start_position:
+                duration -= original_regions_start_position - start
+            if start + duration > original_regions_end_position:
+                duration -= start + duration - original_regions_end_position
+            if original_regions_start_position <= start < original_regions_end_position:
+                reused_event_start_position = reused_start_position + start - original_regions_start_position
+            midi_file.addNote(track_number, channel, midi_event.pitch, reused_event_start_position, duration, velocity)
+        for event in region.musicalEvents:
+            event_start_position = calculate_event_start_time(midi_file, music_ml_meta, event) + reused_start_position
+            if event_start_position > reused_start_position + region.size:
+                raise TextXSemanticError('Musical event is out of region range',
+                                         **get_location(region))
+            bar_position = 0
+            while bar_position_in_ticks(music_ml_model, midi_file, bar_position) < event_start_position:
+                bar_position += 1
+            compile_music_event(music_ml_model, music_ml_meta, event, bar_position, midi_file, track_number, channel,
+                                velocity)
 
 
 def compile_music_event(music_ml_model, music_ml_meta, music_event, position, midi_file, track_number, channel,
@@ -484,7 +541,6 @@ def calculate_music_events_length(midi_file, music_ml_meta, music_events):
     ticks_per_quarternote = midi_file.ticks_per_quarternote
     total_duration = 0
     smallest_start_time = float('inf')
-
     for music_event in music_events:
         if textx_isinstance(music_event, music_ml_meta['Note']):
             if textx_isinstance(music_event, music_ml_meta['Chord']):
