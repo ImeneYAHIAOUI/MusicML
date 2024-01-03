9f07fdfd7ae5a7fd2effcbcde491fc24d1420313
ImeneYAHIAOUI <90778346+ImeneYAHIAOUI@users.noreply.github.com>
2024-01-03 20:17:22 +0100

    replace dirname with adsolute paths

--- MusicMl/src/musicML.py
+++ MusicMl/src/musicML.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from os.path import dirname, join
+import os
 from argparse import ArgumentParser
 from textx import metamodel_from_file
 from textx.export import metamodel_export, model_export
@@ -8,8 +8,10 @@ from generator import generate_midi_file
 
 
 def main(debug=False):
-    this_folder = dirname(__file__)
-    parent_folder = dirname(this_folder)
+    this_folder = os.getcwd()
+    this_folder = this_folder.replace('\\', '/')
+    parent_folder = os.path.abspath(os.path.join(this_folder, os.pardir))
+    parent_folder = parent_folder.replace('\\', '/')
     argparse = ArgumentParser()
     argparse.add_argument('--generate', type=str, help='Generate Midi file from this musicML file')
     argparse.add_argument('-d', '--debug', action='store_true', help='debug mode')
@@ -18,13 +20,12 @@ def main(debug=False):
     ml_file = argparse.parse_args().generate
 
     # Get metamodel from language description
-    music_ml_meta = metamodel_from_file(join(this_folder, 'musicML.tx'),
-                                        debug=debug)
+    music_ml_meta = metamodel_from_file( this_folder + '/musicML.tx', debug=debug)
     # Optionally export meta-model to dot
-    metamodel_export(music_ml_meta, join(this_folder, '../diagram/musicML.dot'))
+    metamodel_export(music_ml_meta, parent_folder + '/diagram/musicML.dot')
 
     # Instantiate model
-    music_ml_model = music_ml_meta.model_from_file(join(parent_folder, ml_file))
+    music_ml_model = music_ml_meta.model_from_file(parent_folder + '/' + ml_file)
 
     ml_file_name = ml_file.split('.')[0]
     try:
@@ -33,10 +34,10 @@ def main(debug=False):
         ml_file_name = ml_file_name.split('\\')[1]
 
     # Generate Midi file
-    generate_midi_file(music_ml_meta, music_ml_model, './generated/' + ml_file_name)
+    generate_midi_file(music_ml_meta, music_ml_model, '../generated/' + ml_file_name)
 
     # Optionally export model to dot
-    model_export(music_ml_model, join(this_folder, '../diagram/' + ml_file_name + '.dot'))
+    model_export(music_ml_model, parent_folder + '/diagram/' + ml_file_name + '.dot')
 
 
 if __name__ == '__main__':
