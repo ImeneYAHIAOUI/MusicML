bffeed5c12ea7d8b354e7f0ed9bc306de7eb9849
Sourour GAZZEH <79751594+sourour9@users.noreply.github.com>
2024-01-05 18:58:43 +0100

    reorganise scripts

--- scenario1.sh
+++ scenario1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Generating Billie jean music..."
-cd src
+cd MusicMl/src
 python musicML.py --generate scenarios/BillieJeanDrum.mml
 
 echo "Billie jean music generation completed."
