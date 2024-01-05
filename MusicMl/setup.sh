964b8774218e87bdb792e714cfe9e3b61e6927ab
Sourour GAZZEH <79751594+sourour9@users.noreply.github.com>
2024-01-05 18:36:37 +0100

    add setup script

---
+++ MusicMl/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+check_installation() {
+    package=$1
+    if python -c "import $package" 2>/dev/null; then
+        echo "$package is already installed."
+    else
+        echo "$package is not installed. Installing now..."
+        pip install $package
+    fi
+}
+
+packages=("midiutil" "textx")
+
+for package in "${packages[@]}"; do
+    check_installation $package
+done
+
+echo "All installations are complete."
