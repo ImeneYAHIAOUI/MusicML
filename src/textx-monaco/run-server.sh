b3dbbb215e4bca76ea38042ce4bdc904d369057e
Sourour GAZZEH <79751594+sourour9@users.noreply.github.com>
2024-01-09 18:41:39 +0100

    fix in monaco editor

--- src/textx-monaco/run-server.sh
+++ src/textx-monaco/run-server.sh
@@ -2,7 +2,6 @@
 
 # Couleurs
 RED='\033[0;31m'
-GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
@@ -18,5 +17,6 @@ echo -e "${BLUE}Changing directory to '$PROJECT_DIR'...${NC}"
 
 echo -e "${BLUE}Executing server.py ...${NC}"
 
-echo -e "${BLUE}Server started ${NC}"
+echo -e "${BLUE}Server started.${NC}"
+echo -e "${RED}Please do not close this terminal; otherwise, the editor won't be able to detect our syntax.${NC}"
 python -W ignore::DeprecationWarning server.py
